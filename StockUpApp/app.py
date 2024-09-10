c331878e48eb8b415b9331fb61da66f91bd47a25
Cyrilzak02 <cyrilzakhia@hotmail.com>
2024-09-09 21:10:36 -0300

    GetAlunos Added

--- StockUpApp/app.py
+++ StockUpApp/app.py
@@ -1,15 +1,29 @@
 from idlelib.macosx import addOpenEventSupport
 
-from flask import Flask
+from flask import Flask, jsonify
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_PORT']=3306
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']='1234'
+app.config['MYSQL_DB']='pucaronas'
+
+mysql = MySQL(app)
+
+@app.route('/getAlunos')
+def getAlunos():  # put application's code here
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM aluno")
+    data = cur.fetchall()
+    cur.close()
+    return jsonify(data)
+
+
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-@app.route('/fuck')
 def fuck():
     return 'Fuck'
 
